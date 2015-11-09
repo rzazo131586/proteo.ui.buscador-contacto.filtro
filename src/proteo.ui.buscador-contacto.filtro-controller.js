8615fee4a058f1b36d0736490e9544d969d5a6c9
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-09 08:50:10 +0100

    controller >> add scope.filter

--- src/proteo.ui.buscador-contacto.filtro-controller.js
+++ src/proteo.ui.buscador-contacto.filtro-controller.js
@@ -3,6 +3,7 @@
 
 angular.module('proteo.ui.buscador-contacto.filtro').controller('buscadorContactoFiltroCtrl', function($scope,personService) {
 	// Code fo Controller
+	$scope.filter={};
 });
 
 }());
