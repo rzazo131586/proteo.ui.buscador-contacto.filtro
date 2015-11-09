d71aa4c6b81fa396cf8c7793486bf2daf4ce58f6
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-09 11:13:31 +0100

    Controller >> Call service && return JSon with Filter && Return JSON to Controller SPA

--- src/proteo.ui.buscador-contacto.filtro-controller.js
+++ src/proteo.ui.buscador-contacto.filtro-controller.js
@@ -5,6 +5,38 @@
 		// Code fo Controller
 		$scope.filter={};
 		$scope.datatitle = "AGENDA DE CONTACTOS";
+
+		$http.get('http://localhost:3000/users').success(function(data) {
+            $scope.contacts 		= data;
+            $scope.contactsFiltered = data;
+        });
+        
+        // Reset INPUTS & LIST constacts ( future : service )
+        $scope.contactReset = function(){
+        	// reasign data into contactsFiltered for reset list contacts
+			$scope.contactsFiltered = $scope.contacts;
+        }
+
+        // Validate if Inputs are empty && search into result service compare with filter
+        $scope.contactFiltered = function (){
+        	$scope.contactsFiltered = [];
+
+        	for(var i = 0; i < $scope.contacts.length ; i++)
+			{	
+				if ($scope.filter.username == undefined && $scope.filter.name == undefined && $scope.filter.company == undefined){
+					// no data in any input 
+					$scope.contactsFiltered = $scope.contacts;
+					return;
+				}else{
+					// validate its username like to name
+					if( $scope.contacts[i].username.toLowerCase().indexOf($scope.filter.username.toLowerCase()) != -1 || 
+						$scope.contacts[i].name.toLowerCase().indexOf($scope.filter.name.toLowerCase()) != -1 ||
+						$scope.contacts[i].company.name.toLowerCase().indexOf($scope.filter.company.toLowerCase()) != -1 ){
+							$scope.contactsFiltered.push($scope.contacts[i]);
+						}
+				}
+       	 	};
+ 		}
 	});
 
 }());
--- src/proteo.ui.buscador-contacto.filtro-directive.js
+++ src/proteo.ui.buscador-contacto.filtro-directive.js
@@ -4,15 +4,15 @@
 	angular.module('proteo.ui.buscador-contacto.filtro').directive('buscadorContacto.filtro',function() {
 	  return {
 		scope: {
-			filterCallback : "="
+			contactsResult : "="
 		},	    
 		restrict	: 	"E",
 	    controller 	: 	'buscadorContactoFiltroCtrl',
 	    templateUrl : 	'./bower_components/proteo.ui.buscador-contacto.filtro/dist/proteo.ui.buscador-contacto.filtro.tpl.html',
 	    link: function($scope){
 			$scope.returnItemsFilter = function(){
-				console.log("Filter >> "+$scope.filter);
-	       		$scope.filterCallback($scope.filter);
+				console.log("contactsFiltered || "+$scope.contactsFiltered);
+	       		$scope.contactsResult($scope.contactsFiltered);
 	 		};
 		}
 	  };
