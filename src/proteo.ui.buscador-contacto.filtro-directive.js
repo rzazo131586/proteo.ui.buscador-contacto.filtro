d9a3a5cf83ef59720917353136fc7e927ce1bd1f
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-09 08:50:31 +0100

    directive >> add logic directive

--- src/proteo.ui.buscador-contacto.filtro-directive.js
+++ src/proteo.ui.buscador-contacto.filtro-directive.js
@@ -1,13 +1,21 @@
 (function () {
     'use strict';
 
-angular.module('proteo.ui.buscador-contacto.filtro').directive('buscadorContacto.filtro',function() {
-  return {
-    scope 		: 	{},
-    restrict	: 	"E",
-    controller 	: 	'buscadorContactoFiltroCtrl',
-    templateUrl : 	'src/proteo.ui.buscador-contacto.filtro.tpl.html'
-  };
-});
+	angular.module('proteo.ui.buscador-contacto.filtro').directive('buscadorContacto.filtro',function() {
+	  return {
+		scope: {
+			filterCallback : "="
+		},	    
+		restrict	: 	"E",
+	    controller 	: 	'buscadorContactoFiltroCtrl',
+	    templateUrl : 	'./bower_components/proteo.ui.buscador-contacto.filtro/dist/proteo.ui.buscador-contacto.filtro.tpl.html',
+	    link: function($scope){
+			$scope.returnItemsFilter = function(){
+				console.log("Filter >> "+$scope.filter);
+	       		$scope.filterCallback($scope.filter);
+	 		};
+		}
+	  };
+	});
 
 }());
