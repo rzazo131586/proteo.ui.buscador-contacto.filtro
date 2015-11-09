ab0534023a8794f05e20a72dc8f66825a7af4784
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-09 08:59:30 +0100

    Add Title of directive

--- dist/proteo.ui.buscador-contacto.filtro.js
+++ dist/proteo.ui.buscador-contacto.filtro.js
@@ -13,6 +13,7 @@
 angular.module('proteo.ui.buscador-contacto.filtro').controller('buscadorContactoFiltroCtrl', function($scope,personService) {
 	// Code fo Controller
 	$scope.filter={};
+	$scope.datatitle = "AGENDA DE CONTACTOS";
 });
 
 }());
--- src/proteo.ui.buscador-contacto.filtro-controller.js
+++ src/proteo.ui.buscador-contacto.filtro-controller.js
@@ -4,6 +4,7 @@
 angular.module('proteo.ui.buscador-contacto.filtro').controller('buscadorContactoFiltroCtrl', function($scope,personService) {
 	// Code fo Controller
 	$scope.filter={};
+	$scope.datatitle = "AGENDA DE CONTACTOS";
 });
 
 }());
